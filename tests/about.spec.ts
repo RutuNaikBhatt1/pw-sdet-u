a496317e6ac593f75ae43ed6426fb4a7b8a797ed
RutuBhatt <rutu.a.bhatt@gmail.com>
2024-04-18 23:31:23 +0100

    Upload test

--- tests/about.spec.ts
+++ tests/about.spec.ts
@@ -2,10 +2,11 @@ import {test,expect} from '@playwright/test';
 import AboutPage from '../pages/aboutPage';
 test.describe('About Page', () => {
     let aboutPage : AboutPage
+    
     test.beforeEach(async ({ page }) => {
      aboutPage = new AboutPage(page);
        await aboutPage.navigate();
-        })
+    })
     
     test('Verify About Page title', async ({ page }) => {
         await expect(page).toHaveTitle('About – Practice E-Commerce Site')
